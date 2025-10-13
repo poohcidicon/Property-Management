a8b10c8821fba48d3e250738512f5f5e69782d86
poohcidicon <111724148+poohcidicon@users.noreply.github.com>
2025-10-13 20:50:49 +0700

    Create checkin.ts

---
+++ lib/api/hotel/checkin.ts
@@ -0,0 +1,72 @@
+import { ApiResponse, axiosPublic } from "@/lib/axios";
+
+export interface IPayloadBookUnitHotel {
+  unit_id: string;
+  booking_date: string;
+  start_date: string;
+  end_date: string;
+  booking_id: string;
+  book_room_id: string;
+}
+export const BookUnitHotelApi = async (payload: IPayloadBookUnitHotel): Promise<ApiResponse<boolean>> => {
+  try{
+    const res = await axiosPublic.post<ApiResponse<boolean>>('/api/hotel/book-unit', payload);
+    return res.data
+  }
+  catch (error: any) {
+    console.error('Error fetching circles:', error);
+    return {
+      success: false,
+      data: false,
+      error: error.message || 'Error fetching circles',
+      message: 'Error fetching circles'
+    }
+  }
+}
+
+export interface IPayloadCheckin {
+  unit_id: string;
+  checkin_date: string;
+  customers: Array<{
+    booking_id: string;
+    book_room_id: string;
+  }>;
+}
+
+export const CheckinUnitApi = async (payload: IPayloadCheckin): Promise<ApiResponse<boolean>> => {
+  try{
+    const res = await axiosPublic.post<ApiResponse<boolean>>('/api/hotel/checkin', payload);
+    return res.data
+  }
+  catch (error: any) {
+    console.error('Error fetching circles:', error);
+    return {
+      success: false,
+      data: false,
+      error: error.message || 'Error fetching circles',
+      message: 'Error fetching circles'
+    }
+  }
+}
+
+export interface IPayloadCheckout {
+  unit_id: string;
+  checkout_date: string;
+  total_amount: number;
+}
+
+export const CheckoutUnitApi = async (payload: IPayloadCheckout): Promise<ApiResponse<boolean>> => {
+  try{
+    const res = await axiosPublic.post<ApiResponse<boolean>>('/api/hotel/checkout', payload);
+    return res.data
+  }
+  catch (error: any) {
+    console.error('Error fetching circles:', error);
+    return {
+      success: false,
+      data: false,
+      error: error.message || 'Error fetching circles',
+      message: 'Error fetching circles'
+    }
+  }
+}
