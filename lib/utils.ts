d08e867d028dfec3dc153a056bde188c39c3c8ab
poohcidicon <111724148+poohcidicon@users.noreply.github.com>
2025-10-29 22:57:36 +0700

    add formatBuddhist

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format } from 'date-fns'
+import { th } from 'date-fns/locale'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatBuddhist(date: Date, pattern: string = 'd MMMM yyyy'): string {
+  const formatted = format(date, pattern, { locale: th })
+  return formatted.replace(/\d{4}$/, (year) => String(parseInt(year) + 543))
 }
