1636d1ae04d3469e8027eff5c2ef958e1cfdcf8b
poohcidicon <111724148+poohcidicon@users.noreply.github.com>
2025-11-12 21:13:25 +0700

    Update guest.ts

--- services/external/hotel/guest.ts
+++ services/external/hotel/guest.ts
@@ -21,7 +21,7 @@ export const getGuestList = async (payload: IPayloadGetGuestListService): Promis
       LEFT JOIN Sys_Hotel_CheckIn booking ON (g.BookingID = booking.BookingID AND g.BookRoomID = booking.BookRoomID and booking.Status = 'W')
       LEFT JOIN Sys_Hotel_CheckIn checkin ON (g.BookingID = checkin.BookingID AND g.BookRoomID = checkin.BookRoomID and checkin.Status = 'A')
       INNER JOIN Sys_Hotel_BookRoom gbr ON (g.BookRoomID = gbr.BookRoomID)
-      WHERE g.CheckIn = @CheckInDate and gbr.Status <> 'P'
+      WHERE g.CheckIn = @CheckInDate and gbr.Status <> 'P' and gbr.Status <> 'O'
     `
     const result = await pool.request()
       .input("CheckInDate", payload.checkin_date || null)
