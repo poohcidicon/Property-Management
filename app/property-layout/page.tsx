e8ef353f904c53b363099e82c6000a6770500bbf
poohcidicon <111724148+poohcidicon@users.noreply.github.com>
2025-11-14 00:14:13 +0700

    Comment out room type reset on dialog close

--- app/property-layout/page.tsx
+++ app/property-layout/page.tsx
@@ -1105,7 +1105,7 @@ export default function PropertyLayout({ typeBusiness, projectId }: PropertyLayo
     // Clear selected property when dialog closes
     setSelectedProperty(null)
     // Reset selected room type to stop blinking animation
-    setSelectedRoomType(null)
+    // setSelectedRoomType(null)
     setShowHotelRoomDialog(false)
     window.dispatchEvent(new CustomEvent("selectedPropertyChanged", { detail: null }))
   }
